39914a9a0912f745accbb766011ff80b696ac610
maxpo-dev <dev.maxpo@gmail.com>
2025-06-05 17:44:04 +0530

    fix: news and blogs changed tp news

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -104,7 +104,7 @@ export default function Navbar() {
     ],
     more: [
       { href: "/market-outlook", label: "Market Outlook" },
-      { href: "/news&blogs", label: "News & Blogs" },
+      { href: "/news", label: "News & Blogs" },
       { href: "/testimonial", label: "Testimonials" },
       { href: "/faqs", label: "FAQs" },
     ],
