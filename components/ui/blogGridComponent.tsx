212942f9dea8f1d39700c5350054d65e54d92c42
maxpo-dev <dev.maxpo@gmail.com>
2025-06-11 14:36:03 +0530

    category removed

--- components/ui/blogGridComponent.tsx
+++ components/ui/blogGridComponent.tsx
@@ -6,20 +6,17 @@ import { useRouter } from "next/navigation";
 
 interface BlogGridClientProps {
   posts: any[];
-  categories: string[];
 }
 
 export default function BlogGridClient({
   posts,
-  categories,
 }: BlogGridClientProps) {
-  const [selectedCategory, setSelectedCategory] = useState("View All");
 
   const router = useRouter();
   return (
     <div>
       {/* Header + Filters */}
-      <div className="bg-black px-4 sm:px-6 md:px-10 lg:px-20 py-8 flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
+      <div className="  bg-black px-4 sm:px-6 md:px-10 lg:px-20 py-8 flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
         <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-white">
           News & Blogs
         </h2>
@@ -45,7 +42,7 @@ export default function BlogGridClient({
       </div>
 
       {/* Blog Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 sm:px-6 md:px-10 lg:px-28 py-10">
+      <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 sm:px-6 md:px-10 lg:px-28 py-10">
         {posts.map((article, idx) => (
           <div
             key={article._id}
--- sanity/lib/blogs.ts
+++ sanity/lib/blogs.ts
@@ -9,20 +9,31 @@ import { SanityDocument } from "next-sanity";
 export async function getBlogPostsByProject(
   slug: string
 ): Promise<SanityDocument[]> {
-  return await client.fetch(blogPostsByProject, { slug },{ cache: 'no-store' });
+  return await client.fetch(
+    blogPostsByProject,
+    { slug },
+    { cache: "no-store" }
+  );
 }
 
-export async function getCategoriesByProject(slug: string): Promise<string[]> {
-  const rawCategories: string[] = await client.fetch(categoriesByProject, {
-    slug,
-    
-  },{ cache: 'no-store' });
-  const cleaned = rawCategories.filter((c): c is string => !!c);
-  const unique = Array.from(new Set(cleaned));
-  return unique;
-}
+// export async function getCategoriesByProject(slug: string): Promise<string[]> {
+//   const rawCategories: string[] = await client.fetch(
+//     categoriesByProject,
+//     {
+//       slug,
+//     },
+//     { cache: "no-store" }
+//   );
+//   const cleaned = rawCategories.filter((c): c is string => !!c);
+//   const unique = Array.from(new Set(cleaned));
+//   return unique;
+// }
 
 export async function getBlogPostBySlug(slug: string) {
-  const post = await client.fetch(blogPostBySlug, { slug },{ cache: 'no-store' });
+  const post = await client.fetch(
+    blogPostBySlug,
+    { slug },
+    { cache: "no-store" }
+  );
   return post;
 }
